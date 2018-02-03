dbd4a868af45918059480bf1f33eec2b48c41a9f
wentaotang <wentao_tang@126.com>
2018-02-03 22:24:56 +0800

    新增优雅下线服务功能

---
+++ spring-cloud-stream-demo/src/main/java/com/hgcode/cloud/stream/StreamdemoApplication.java
@@ -0,0 +1,29 @@
+package com.hgcode.cloud.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.support.GenericMessage;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SpringBootApplication
+@EnableBinding(Source.class)
+public class StreamdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamdemoApplication.class, args);
+    }
+
+    @Bean
+    @InboundChannelAdapter(value = Source.OUTPUT)
+    public MessageSource<String> timerMessageSource() {
+        return () -> new GenericMessage<>(new SimpleDateFormat().format(new Date()));
+    }
+
+}
