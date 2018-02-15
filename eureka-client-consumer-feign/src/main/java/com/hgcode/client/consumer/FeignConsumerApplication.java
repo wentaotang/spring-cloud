23d1331643746cf34b705d90d6dab1da3722efee
wentaotang <wentao_tang@126.com>
2018-02-15 14:11:50 +0800

    update

--- eureka-client-consumer-feign/src/main/java/com/hgcode/client/consumer/FeignConsumerApplication.java
+++ eureka-client-consumer-feign/src/main/java/com/hgcode/client/consumer/FeignConsumerApplication.java
@@ -2,20 +2,16 @@ package com.hgcode.client.consumer;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ConsumerApplication {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+@EnableFeignClients
+public class FeignConsumerApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ConsumerApplication.class).web(true).run(args);
+        new SpringApplicationBuilder(FeignConsumerApplication.class).web(true).run(args);
     }
 }
