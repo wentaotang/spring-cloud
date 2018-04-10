af8b077ff2beaee12182a5956feb2657dcd555fd
wentaotang <=wentao_tang@126.com>
2018-04-10 15:46:54 +0800

    eureka 集群配置demo

--- eureke-client-provider/src/main/java/com/hgcode/client/provider/ClientProviderApplication.java
+++ eureke-client-provider/src/main/java/com/hgcode/client/provider/ClientProviderApplication.java
@@ -1,5 +1,6 @@
 package com.hgcode.client.provider;
 
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ClientProviderApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ClientProviderApplication.class).web(true).run(args);
+        new SpringApplicationBuilder(ClientProviderApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
