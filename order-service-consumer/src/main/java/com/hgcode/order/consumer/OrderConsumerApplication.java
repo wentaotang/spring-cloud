a51e74466e74b26ad696cec0db7a40f806d89678
wentaotang <wentao_tang@126.com>
2018-04-14 20:43:49 +0800

    update

--- order-service-consumer/src/main/java/com/hgcode/order/consumer/OrderConsumerApplication.java
+++ order-service-consumer/src/main/java/com/hgcode/order/consumer/OrderConsumerApplication.java
@@ -4,11 +4,11 @@ package com.hgcode.order.consumer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
--- order-service-consumer/src/main/java/com/hgcode/order/consumer/client/OrderClient.java
+++ order-service-consumer/src/main/java/com/hgcode/order/consumer/client/OrderClient.java
@@ -4,7 +4,6 @@ package com.hgcode.order.consumer.client;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import java.util.Map;
 
 @FeignClient(name = "order-service")
