30294589c417feb8d1751df26565de146ce6ed8b
wentaotang <=wentao_tang@126.com>
2018-04-19 14:26:56 +0800

    update

--- order-service-client/src/main/java/com/hgcode/order/client/OrderClientApplication.java
+++ order-service-client/src/main/java/com/hgcode/order/client/OrderClientApplication.java
@@ -2,10 +2,14 @@ package com.hgcode.order.client;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrix
 public class OrderClientApplication {
 
     public static void main(String[] args) {
--- order-service-consumer/src/main/java/com/hgcode/order/consumer/OrderConsumerApplication.java
+++ order-service-consumer/src/main/java/com/hgcode/order/consumer/OrderConsumerApplication.java
@@ -3,10 +3,12 @@ package com.hgcode.order.consumer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 
 @EnableFeignClients
 @SpringCloudApplication
+@EnableHystrixDashboard
 public class OrderConsumerApplication {
 
     public static void main(String[] args) {
