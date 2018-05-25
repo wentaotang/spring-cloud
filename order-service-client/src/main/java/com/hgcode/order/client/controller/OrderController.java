7cc5d8202df2864ff0e92e43ffc80380e85441d2
wentaotang <wentao_tang@126.com>
2018-05-25 17:28:01 +0800

    update

--- order-service-client/src/main/java/com/hgcode/order/client/controller/OrderController.java
+++ order-service-client/src/main/java/com/hgcode/order/client/controller/OrderController.java
@@ -1,7 +1,7 @@
 package com.hgcode.order.client.controller;
 
 import com.hgcode.order.client.vo.User;
-import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,7 @@ public class OrderController {
     @RequestMapping("/save")
     public String save(@RequestBody Map<String,Object> map) throws InvocationTargetException, IllegalAccessException {
         User user=new User();
-        BeanUtils.populate(user,map);
+        BeanUtils.copyProperties(user,map);
         System.out.println(user);
         return "ok";
     }
