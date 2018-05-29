433aab773ea1b673b441d35b3186e046d28e248f
wentaotang <wentao_tang@126.com>
2018-05-29 17:14:16 +0800

    update

---
+++ crawler-demo/demo.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `info` (
+  `id` varchar(100) DEFAULT NULL
+  `title` varchar(100) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `source` varchar(100) DEFAULT NULL,
+  `scrapy_source` varchar(100) DEFAULT NULL,
+  `url` varchar(100) DEFAULT NULL,
+  `image_url` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
